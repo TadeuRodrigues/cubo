fc40ebfab0b89f479369d0167af253fb1e358042
Tadeu Rodrigues <karlostadeu85@gmail.com>
2014-10-19 02:27:21 +0000

    Adding puma with default server

---
+++ config/puma.rb
@@ -0,0 +1,10 @@
+# config/puma.rb
+threads 4,16
+workers 2
+preload_app!
+port 3000
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
